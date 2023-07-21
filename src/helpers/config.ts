16a70acc1eb2592a89df66bf7fb08702935c641c
Kenny3231 <kennyslayer3269@gmail.com>
2023-07-21 17:20:25 +0200

    Update config.ts

--- src/helpers/config.ts
+++ src/helpers/config.ts
@@ -91,12 +91,12 @@ const config: SiteConfig = {
   },
   workExperience: [
     {
-      company: 'AND.Digital',
+      company: 'Ici',
       companyUrl: 'https://and.digital/',
       companyLogo: 'https://and.digital/favicon.ico',
-      jobTitle: 'Principle Engineer',
-      datesWorked: 'August 2022 - Present',
-      projectType: 'Clients',
+      jobTitle: 'Le Poste',
+      datesWorked: 'Janvier 2021 - Present',
+      projectType: 'Muti-Clients',
       projects: [
         { name: 'Frasers Group', logo: 'https://i.ibb.co/sQcvxmG/Frasers.png', description: '', },
         { name: 'Lloyds Bank', logo: 'https://i.ibb.co/GCSDPCq/Lloyds-bank.jpg', description: '', },
@@ -104,52 +104,7 @@ const config: SiteConfig = {
       technologies: [ 'TypeScript', 'React Native', 'Azure' ],
     },
     {
-      company: 'Galaxy',
-      companyUrl: 'https://galaxy.com/',
-      companyLogo: 'https://i.ibb.co/fX2rF84/galaxy-digital.png',
-      jobTitle: 'VP Frontend Engineer',
-      datesWorked: 'Jan 2021 - July 2022',
-      responsibilities: 'Worked on real-time React-based Web3 trading application',
-      technologies: [ 'React', 'StoryBook', 'Cypress', 'web3js',],
-    },
-    {
-      company: 'Accenture',
-      companyUrl: 'https://www.accenture.com/',
-      companyLogo: 'https://i.ibb.co/kcCKDF1/accenture.png',
-      jobTitle: 'Software Developer',
-      datesWorked: 'June 2014 - February 2019',
-      responsibilities: ''
-        + 'After joining as an associate, I had the opportunity to work on a range of exciting client projects, '
-        + 'each with their own challenges, his helped me to develop my skills and knowledge in a variety of areas. '
-        + 'Over time I worked my way up to a senior role, where I could actively deliver high quality solutions to clients.',
-        projectType: 'Clients',
-        projects: [
-        { name: 'E.ON', logo: 'https://i.ibb.co/4PcVN4B/Eon.png', description: '', },
-        { name: 'B&Q', logo: 'https://i.ibb.co/F79xSDK/Bandq.jpg', description: '', },
-        { name: 'Boots', logo: 'https://i.ibb.co/SnPtC8t/Boots.jpg', description: '', },
-        { name: 'TK Maxx', logo: 'https://i.ibb.co/MBkQVs1/tkmaxx.jpg', description: '', },
-        { name: 'Vodafone', logo: 'https://i.ibb.co/Zh1SwbR/Vodafone.png', description: '', },
-        { name: 'UKI Innovation Center', logo: 'https://i.ibb.co/1JX4Q9Y/Accenture-innovation.jpg', description: '', },
-      ],
-      technologies: [ 'C#', 'Angular', 'Node', 'AWS' ],
-    },
-    {
-      company: 'University of Oxford',
-      companyUrl: 'https://www.ox.ac.uk/',
-      companyLogo: 'https://i.ibb.co/QYmLPTX/oxford-uni.jpg',
-      jobTitle: 'Intern',
-      datesWorked: 'June 2013 - September 2013',
-      responsibilities: 'Worked on developing a Java application which would calculate the most '
-        + 'effective mg of antimalarial drug, per kg or patient body weight, to prevent vaccine resistance building up. '
-        + 'Involved dealing with millions of patient records, which were often in varying formats and units.',
-        projectType: 'Projects',
-        projects: [
-        { name: 'WWARN', logo: 'https://i.ibb.co/1R657nh/Wwarn.jpg', description: '', },
-      ],
-      technologies: [ 'Java', 'apachetomcat', 'junit', 'gradle' ],
-    },
-    {
-      company: 'British Army',
+      company: 'Armée',
       companyUrl: 'https://www.army.mod.uk/',
       companyLogo: 'https://i.ibb.co/gv6mbKw/Army.jpg',
       jobTitle: 'Soldier',
@@ -267,71 +222,19 @@ const config: SiteConfig = {
     { postRef: '', thumbnail: '' },
   ],
   contact: {
-    name: 'Alicia Sykes',
-    email: 'alicia@omg.lol',
+    name: 'DD',
+    email: 'dd@gmail.com',
     website: 'https://aliciasykes.com',
     socials: {
-      Twitter: 'Lissy_Sykes',
-      GitHub: 'Lissy93',
-      'Dev.to': 'Lissy93',
-      Mastodon: '@Lissy93',
-      LinkedIn: 'in/AliciaSykes',
-      Instagram: 'liss.sykes',
-      YouTube: 'AliciaSykes',
-      Reddit: 'lissy93',
-      StackOverflow: '979052/alicia',
-      KeyBase: 'AliciaSykes',
-      Facebook: 'Liss.Sykes',
-      PeerList: 'alicia',
-      CodersRank: 'lissy93',
-      'OMG.lol': 'alicia',
+      LinkedIn: 'in/david-%F0%9F%94%B4%E2%9A%AB-dehos-2b51a088/',
     },
-    socialButtonLimit: 6,
+    socialButtonLimit: 2,
     mailerConfig: {
       service_id: 'gmail',
       template_id: 'website-contact-form',
       user_id: 'user_K6F041q1dQvz8SJLSOZCR',
       template_params: {},
     },
-    pgpKeyLink: 'https://keybase.io/aliciasykes/pgp_keys.asc?fingerprint=0688f8d34587d954e9e51fb8fedb68f55c0283a7',
-    pgpPublicKey: `-----BEGIN PGP PUBLIC KEY BLOCK-----
-      mQENBFqbwpsBCACxoSZKSkr6zPUSVijbeFV9c7KphqXJxzJqKlWQbMOFL+rj52+Q
-      F/wy16+Jze4seOGUNA9OiOcpM/YhSQoeFOBPdj5hOc9IXOTx86bhYi3+84D9o1cW
-      Qq80sOeRNwOzjH7539tttQmBPpFTBAIJYa1l/UP2CWBm2cUk7/f/g/pEaCvdqayp
-      0uAl3EfNbYkjqw+0GawIjY34Xqza+MONooqDXDJC//xnWDb6UTzTPy3LVX8PMmiw
-      Gkt6+3Xlfro+h2Y4AlzvVMjvkBWqJwHa6K4YOUsw6gtxZ/W3UIx6ECM5COa/apX/
-      o3F47k+SVduHb0vfNuHQva+k3Rk7WQlSCCGZABEBAAG0OWFsaWNpYS5zeWtlc0Bw
-      cm90b25tYWlsLmNvbSA8YWxpY2lhLnN5a2VzQHByb3Rvbm1haWwuY29tPokBPwQQ
-      AQgAKQUCWpvCnAYLCQcIAwIJEP7baPVcAoOnBBUICgIDFgIBAhkBAhsDAh4BAAoJ
-      EP7baPVcAoOnRbIH/jMstIMLFSwWhTclFr8idbjMMmmcxOoZ7UtwNOKf3kAbSsZ8
-      qpQgBYIuN2im3W+WcgM4uqgu4daAXagl6Z0+MsRyPm0ULKVPAmQZuiaxhoXrDsK8
-      I/7on5JuU9100pav2GSpDGvWlJj4S91kDYSKA3BqzCwNws5taKYrYfO/2ZgFwx2L
-      LG0Zf/V5afBc3wAZUKTqy+4elT9O3XutURdYBatJtgrqlx5p4vFv7neIlaTo1i9m
-      BdlsupJDzqUoog6W/vTbLVbgVIBYgYiI1rV9UOD3Ds2y5RMRvKGUheKpc8dATCxD
-      W8aHT+I89GhLg9qj6rGDfUYwXmTo/ZD0lo8xH8a0KkFsaWNpYSBTeWtlcyA8YWxp
-      Y2lhLnN5a2VzQHByb3Rvbm1haWwuY29tPokBTgQTAQgAOBYhBAaI+NNFh9lU6eUf
-      uP7baPVcAoOnBQJe5omDAhsDBQsJCAcCBhUKCQgLAgQWAgMBAh4BAheAAAoJEP7b
-      aPVcAoOnI0QIAKmELC7E3d4qvL9pK9mc/GKag9NGZApvQwuTvdoTHq2ggF7YCVZT
-      atdPlwKbpYbOfyg5XG+Q2L9oZ6cieilxBSzurK35hZG5vj5s02SNnRb4PAZXiZje
-      Z7B6zxzGmxVelsnRQVVIE+dfGvaX8MbXCNJliL2lvu4oE+W3pBZfiMe1ahL3TLGo
-      hiCgx1f7Yvrx9NXlouFdZAFnUWrwG9Uk7R6ZYrjvSM+zhNWjZQRroNIem1Fsn5fX
-      EcStRazzbLvIib4x0+0M+D4yRBEX0MGRF4VMDksW4rV9QiMnRoe5QZbITtTFuT4r
-      cn1ZH6iQviJuAsfCb/PqKjkUP21b0BJkcZa5AQ0EWpvCmwEIANo4THKI5PMzSzsR
-      I+uf9Lj2nFj0R/Vw7FtWNVzvQ/G2yH/GRc39n3GUWFkW93N+Sqep70aRWwCod2pN
-      tQSYG7qBOk1uL3HRbCUIIf4scb+qjuDMdJPxJPxqEGQaDIAYm8dH5uDq9BQm6xqX
-      0jDgjGWqNprnSYBOU0+29G/jjRmZC3IFmQNByXT4kdkduLOAOws2wMGMAf9XV/eE
-      ED4ZF4s0Syg1MJgx82OcTEA7dzSImBArXgdlc9/MTEoX7D7JtG30r/Q7vU7oj7pa
-      pKfjng2BEw98+jc7qARKz2SMORkmLLAS0mxsCe6QH7cx71cd7ptEHV18iFTNBCoq
-      c1JqTPcAEQEAAYkBKQQYAQgAEwUCWpvCnQkQ/tto9VwCg6cCGwwACgkQ/tto9VwC
-      g6fLWQf+K2HgzOxsKx5CWICoDjqk2zqOT1I7nf5nD7CqQIwP3tUsl+RZu85EyEHY
-      XE30sOx0ZdGoU1fCCsA785gSPG2NObIeVQMOLBMftSv/IO2qZJvl3lfEDfurPZ4K
-      Ywl5lyDR6iF7D96IYrYHInDA53mKDvrSVBF8w1tt2CUS1o2YZUST9061gX7KSEn1
-      FstAf/0TpFnXDbdM/O0gLAkPPCcynWZzX+Ujwsca0xZF3lcRR5xtjCTTdvhbI3PP
-      q7lzS3Uq09Iz0Vb+RtP5TzTlllbhWEREorY5nVRu89rt+zv8SbkqyOmKR0/e7UDj
-      mFFVmpQe2X/qgZBVVYC+LjpxNEMt6w==
-      =dpLK
-      -----END PGP PUBLIC KEY BLOCK-----
-`,
   },
   about: {
     intro: 'Just your average dev...',
